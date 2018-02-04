d5f0c73d081bf7746404a1c81e81378b4fc70361
Changjie <changjieguan@gmail.com>
2018-02-04 19:41:44 +0100

    Add test results and combine images

---
+++ output_images/combineTest.sh
@@ -0,0 +1,3 @@
+montage test1_o.jpg test2_o.jpg test3_o.jpg \
+test4_o.jpg test5_o.jpg test6_o.jpg\
+ -tile 2x3 -geometry 555x328+1+1 output_tests.png 
---
+++ output_images/combine_images.sh
@@ -0,0 +1,1 @@
+montage sb_3.jpg sb_2.jpg sb_1.jpg sb_0_5.jpg -geometry 760x500+2+2 output_scales.png 
